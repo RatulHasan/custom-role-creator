c4dd578eb2a1c4f7061c15be17acb4ecd2668220
ratul <ratuljh@gmail.com>
2021-04-29 06:19:21 +0600

    Vendor folder added.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '23147685a32fcf1d27142ea0b0b3d36483819c9e',
+    'name' => 'ratul/custom-role-creator',
+  ),
+  'versions' => 
+  array (
+    'ratul/custom-role-creator' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '23147685a32fcf1d27142ea0b0b3d36483819c9e',
+    ),
+  ),
+);
