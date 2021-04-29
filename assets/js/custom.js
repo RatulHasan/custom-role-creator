b4194a43d58be4e596fd35257622a523c32a792b
ratul <ratuljh@gmail.com>
2021-04-29 06:17:28 +0600

    Assets added.

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+;(function ($) {
+    $( document ).on(
+        "click",
+        '.crc_role_edit_button',
+        function (e) {
+			var role_name = $( this ).data( 'edit_crc_role_name' );
+			$( "#edit_role_name" ).val( role_name );
+		}
+    );
+
+    /**
+     * Success message
+     */
+    $( "#crc_settings_message" ).delay( 450 ).addClass( "in" ).fadeOut( 6000 );
+})( jQuery );
